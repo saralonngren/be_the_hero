db660db1e00dae782247231085f2d7f46a9c6b6d
Sara Lonngren <saralonngren@gmail.com>
2020-03-25 14:15:05 -0300

    Improved routes, created incidents and ongs controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,24 +1,15 @@
 const express = require('express')
-const crypto = require('crypto')
-const connection = require('./database/connection')
+
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
 
 const routes = express.Router()
 
-routes.post('/ongs', async (request, response) => {
-  const { name, email, whatsapp, city, uf } = request.body
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
 
-  const id = crypto.randomBytes(4).toString('HEX')
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-
-  return response.json({ id })
-})
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes
